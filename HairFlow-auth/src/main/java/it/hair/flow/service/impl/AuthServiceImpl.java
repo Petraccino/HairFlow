87c5f85cd1f3e235117c932d96885c2a8e8df138
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-01-18 22:24:00 +0100

    Pulizia import

--- HairFlow-auth/src/main/java/it/hair/flow/service/impl/AuthServiceImpl.java
+++ HairFlow-auth/src/main/java/it/hair/flow/service/impl/AuthServiceImpl.java
@@ -2,7 +2,6 @@ package it.hair.flow.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
