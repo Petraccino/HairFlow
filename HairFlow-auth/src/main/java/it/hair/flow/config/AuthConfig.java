f6fd0decee41504cbbad546c30d8c8d8011e5e87
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-02-02 17:17:53 +0100

    Insert list of RequestMatcher

--- HairFlow-auth/src/main/java/it/hair/flow/config/AuthConfig.java
+++ HairFlow-auth/src/main/java/it/hair/flow/config/AuthConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import it.hair.flow.config.details.CredentialDetailsService;
 
@@ -19,6 +22,27 @@ import it.hair.flow.config.details.CredentialDetailsService;
 @Configuration
 @EnableWebSecurity
 public class AuthConfig {
+	
+	private static final RequestMatcher PUBLIC_URLS = new OrRequestMatcher(
+			new AntPathRequestMatcher("/auth/register/**"),
+			new AntPathRequestMatcher("/auth/login/**"),
+			new AntPathRequestMatcher("/auth/generate/token"),
+			new AntPathRequestMatcher("/auth/validate/token"),
+            new AntPathRequestMatcher("**/swagger-ui.html"),
+            new AntPathRequestMatcher("/swagger-ui**"),
+            new AntPathRequestMatcher("*/swagger-ui/*"),
+            new AntPathRequestMatcher("/swagger-ui.html"),
+            new AntPathRequestMatcher("*/swagger-ui.html/*"),
+            new AntPathRequestMatcher("*/swagger-ui/index.html/*"),
+            new AntPathRequestMatcher("/swagger-ui/index.html/**"),
+            new AntPathRequestMatcher("/v3/api-docs/**"),
+            new AntPathRequestMatcher("/v3/api-docs**"),
+            new AntPathRequestMatcher("/v3/api-docs.yaml"),
+            new AntPathRequestMatcher("/v3/openapi.yaml"),
+            new AntPathRequestMatcher("/swagger-ui/**"),
+            new AntPathRequestMatcher("/actuator/**"),
+            new AntPathRequestMatcher("/config.json")
+    );
 	
 	@Bean
 	UserDetailsService userDetailsService(){
@@ -34,7 +58,7 @@ public class AuthConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/register/**", "/auth/login/**", "/auth/generate/token", "/auth/validate/token").permitAll()
+                .requestMatchers(PUBLIC_URLS).permitAll()
                 .and()
                 .build();
     }
