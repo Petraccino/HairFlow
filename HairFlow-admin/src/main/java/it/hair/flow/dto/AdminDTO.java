9799b6625a9d69447764fe1640a1c91094c0a6de
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-07-25 15:14:27 +0200

    Update AdminDTO in Admin/Auth service

--- HairFlow-admin/src/main/java/it/hair/flow/dto/AdminDTO.java
+++ HairFlow-admin/src/main/java/it/hair/flow/dto/AdminDTO.java
@@ -1,6 +1,9 @@
 package it.hair.flow.dto;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
 @Data
 public class AdminDTO {
 
@@ -8,6 +11,13 @@ public class AdminDTO {
     private String email;
     private String password;
     private GrantDTO grant;
-    private InformazioniAdminUtenteDTO informazioni;
+    private String nome;
+    private String cognome;
+    private LocalDate dataNascita;
+    private String provincia;
+    private String numeroTelefono;
+    private String sesso;
+    private String codiceFiscale;
+    private OffsetDateTime dataRegistrazione;
 
 }
--- HairFlow-auth/src/main/java/it/hair/flow/dto/UtenteDTO.java
+++ HairFlow-auth/src/main/java/it/hair/flow/dto/UtenteDTO.java
@@ -2,6 +2,9 @@ package it.hair.flow.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
 @Data
 public class UtenteDTO {
 	
@@ -9,5 +12,12 @@ public class UtenteDTO {
  	private String email;
  	private String password;
     private Integer grant;
-    private InformazioniAdminUtenteDTO informazioni;
+	private String nome;
+	private String cognome;
+	private LocalDate dataNascita;
+	private String provincia;
+	private String numeroTelefono;
+	private String sesso;
+	private String codiceFiscale;
+	private OffsetDateTime dataRegistrazione;
 }
