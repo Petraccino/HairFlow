c4b4fdfafc5e2397289b0cb52c4693f232f8e2b4
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-07-25 18:40:32 +0200

    Update DDL_UTENTE_CLIENTE.sql

--- DDL_UTENTE_CLIENTE.sql
+++ DDL_UTENTE_CLIENTE.sql
@@ -1,12 +1,13 @@
 -- hairflow.utente_cliente definition
 
 CREATE TABLE `utente_cliente` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `utente_id` int NOT NULL,
-  `cliente_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_utente_id` (`utente_id`),
-  KEY `FK_cliente_id` (`cliente_id`),
-  CONSTRAINT `FK_utente_id` FOREIGN KEY (`utente_id`) REFERENCES `utente` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `FK_cliente_id` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+                                  `id` int NOT NULL AUTO_INCREMENT,
+                                  `utente_id` int NOT NULL,
+                                  `cliente_id` int NOT NULL,
+                                  PRIMARY KEY (`id`),
+                                  UNIQUE KEY `UK_utente_cliente` (`utente_id`,`cliente_id`),
+                                  KEY `FK_utente_id` (`utente_id`),
+                                  KEY `FK_cliente_id` (`cliente_id`),
+                                  CONSTRAINT `FK_cliente_id` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`) ON DELETE CASCADE,
+                                  CONSTRAINT `FK_utente_id` FOREIGN KEY (`utente_id`) REFERENCES `utente` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
