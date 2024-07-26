e58f0883a87b6f291e92a6eb65b3867b2792c737
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-07-26 12:09:37 +0200

    Update class test User

--- HairFlow-admin/src/test/java/it/hair/flow/HairFlowAdminApplicationTests.java
+++ HairFlow-admin/src/test/java/it/hair/flow/HairFlowAdminApplicationTests.java
@@ -5,14 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
-class HairFlowUserApplicationTests {
+class HairFlowAdminApplicationTests {
 
 	@Container
 	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
@@ -21,26 +20,13 @@ class HairFlowUserApplicationTests {
 			.withPassword("test")
 			.withFileSystemBind("src/main/resources/application-test.yaml", "/usr/share/nginx/html/application-test.yaml", BindMode.READ_ONLY);
 
-	@Container
-	static GenericContainer<?> configServerContainer = new GenericContainer<>("nginx:latest")
-			.withExposedPorts(80)
-			.withFileSystemBind("src/main/resources/application-test.yaml", "/usr/share/nginx/html/application-test.yaml", BindMode.READ_ONLY);
-
-
 	@DynamicPropertySource
 	static void mySQLContainerProperties(DynamicPropertyRegistry registry) {
 		registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
 		registry.add("spring.datasource.username", mySQLContainer::getUsername);
 		registry.add("spring.datasource.password", mySQLContainer::getPassword);
 
-		String configServerUrl = "http://" + configServerContainer.getHost() + ":" + configServerContainer.getMappedPort(80);
-		registry.add("spring.cloud.config.uri", () -> configServerUrl);
-
-		registry.add("eureka.client.registerWithEureka", () -> false);
-		registry.add("eureka.client.fetchRegistry", () -> false);
-
 	}
-
 
 	@Test
 	void contextLoads() {
