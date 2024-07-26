e69836042b52672b04604784bae805490761a4de
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-07-26 12:49:41 +0200

    Update testing for maven install HealtCheck Service

--- HairFlow-healt-check/src/test/java/it/hair/flow/HairFlowHealtCheckApplicationTests.java
+++ HairFlow-healt-check/src/test/java/it/hair/flow/HairFlowHealtCheckApplicationTests.java
@@ -2,9 +2,19 @@ package it.hair.flow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class HairFlowHealtCheckApplicationTests {
+
+	@DynamicPropertySource
+	static void customProperties(DynamicPropertyRegistry registry) {
+		registry.add("eureka.client.registerWithEureka", () -> false);
+		registry.add("eureka.client.fetchRegistry", () -> false);
+	}
 
 	@Test
 	void contextLoads() {
