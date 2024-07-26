4070ac322e7b1c3222edfe34d5176e8ba68bf281
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-07-26 12:58:45 +0200

    Update testing for maven install User Service

--- HairFlow-user/src/test/java/it/hair/flow/HairFlowUserApplicationTests.java
+++ HairFlow-user/src/test/java/it/hair/flow/HairFlowUserApplicationTests.java
@@ -2,9 +2,33 @@ package it.hair.flow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class HairFlowUserApplicationTests {
+
+	@Container
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
+			.withDatabaseName("test")
+			.withUsername("test")
+			.withPassword("test");
+
+	@DynamicPropertySource
+	static void customProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", mySQLContainer::getUsername);
+		registry.add("spring.datasource.password", mySQLContainer::getPassword);
+
+		registry.add("eureka.client.registerWithEureka", () -> false);
+		registry.add("eureka.client.fetchRegistry", () -> false);
+
+	}
 
 	@Test
 	void contextLoads() {
